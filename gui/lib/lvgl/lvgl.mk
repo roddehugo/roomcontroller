a15e890d1869da7fcac100e3ae10ef0acc0e77fe
Hugo Rodde <hugo.rodde@me.com>
2020-04-21 17:14:06 +0200

    gui/lib/lvgl: lv_conf.h shall be defined per project
    In lvgl source files, one can find the following code block:
    
        #ifdef LV_CONF_INCLUDE_SIMPLE
        #include "lv_conf.h"
        #else
        #include "../../../lv_conf.h"
        #endif
    
    As we will have multiple projects with potentially multiple
    configurations, a single lv_conf.h file in the LVGL_DIR cannot satisfy
    our needs. Developers must ass a configuration per project or test.

--- gui/lib/lvgl/lvgl.mk
+++ gui/lib/lvgl/lvgl.mk
@@ -3,5 +3,7 @@ LVGL_DIR = $(BASE)/lib/lvgl
 include $(LVGL_DIR)/lvgl/lvgl.mk
 include $(LVGL_DIR)/lv_drivers/lv_drivers.mk
 
+DEFS += -DLV_CONF_INCLUDE_SIMPLE # 'lv_conf.h' is defined per project.
+
 CFLAGS += -I$(LVGL_DIR)
 SOURCES += $(CSRCS) $(ASRCS)
