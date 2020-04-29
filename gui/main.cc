e1f3d52bd0d828690619770f626cd1fe37e2aa62
Hugo Rodde <hugo.rodde@me.com>
2020-04-29 15:27:47 +0200

    gui: remove unnecessary MONITOR_ZOOM define with default value

--- gui/main.cc
+++ gui/main.cc
@@ -4,16 +4,13 @@
 #include <fstream>
 #include <iostream>
 
-#ifndef MONITOR_ZOOM
-#define MONITOR_ZOOM 1
-#endif
 
 int main(int argc, const char ** argv)
 {
     lv_init();
 
-    SdlDisplay display(LV_HOR_RES, LV_VER_RES, MONITOR_ZOOM);
-    SdlPointer pointer(MONITOR_ZOOM);
+    SdlDisplay display(LV_HOR_RES, LV_VER_RES);
+    SdlPointer pointer;
     Gui gui(display, pointer);
     return gui.loop();
 }
