7114d6d8f29092459abb50f184b286ec2ccbd595
Hugo Rodde <hugo.rodde@me.com>
2020-04-29 15:38:25 +0200

    gui: read and check variable display size

--- gui/main.cc
+++ gui/main.cc
@@ -4,13 +4,21 @@
 #include <fstream>
 #include <iostream>
 
+#include <cassert>
+#define assertm(exp, msg) assert(((void)msg, exp))
 
 int main(int argc, const char ** argv)
 {
     lv_init();
 
-    SdlDisplay display(LV_HOR_RES, LV_VER_RES);
+    int width = LV_HOR_RES;
+    assertm(width <= LV_HOR_RES_MAX, "width is greater than LV_HOR_RES_MAX");
+    int height = LV_VER_RES;
+    assertm(height <= LV_VER_RES_MAX, "height is greater than LV_VER_RES_MAX");
+
+    SdlDisplay display(width, height);
     SdlPointer pointer;
+
     Gui gui(display, pointer);
     return gui.loop();
 }
