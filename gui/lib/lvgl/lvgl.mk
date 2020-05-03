a87e6fd7e94e0e2a5512b02a72f86b962aae287e
Hugo Rodde <hugo.rodde@me.com>
2020-05-03 17:54:53 +0200

    gui/lib/lvgl: optionally include lv_{drivers,examples}.mk

--- gui/lib/lvgl/lvgl.mk
+++ gui/lib/lvgl/lvgl.mk
@@ -2,6 +2,14 @@ LVGL_DIR = $(BASE)/lib/lvgl
 
 include $(LVGL_DIR)/lvgl/lvgl.mk
 
+ifeq ($(LV_DRIVERS),yes)
+include $(LVGL_DIR)/lv_drivers/lv_drivers.mk
+endif
+
+ifeq ($(LV_EXAMPLES),yes)
+include $(LVGL_DIR)/lv_examples/lv_examples.mk
+endif
+
 DEFS += -DLV_CONF_INCLUDE_SIMPLE # 'lv_conf.h' is defined per project.
 
 CFLAGS += -I$(LVGL_DIR)
